c13832cea20b488b5a367dcbc770eb76e01de179
rockhowse <rockhowse@gmail.com>
2015-05-25 20:18:10 -0400

    Added first game dev video code.

---
+++ 01_GD_Py3_PyGame_Intro.py
@@ -0,0 +1,28 @@
+__author__ = 'Nate'
+
+'''
+    pygame tutorial video
+    URL: https://www.youtube.com/watch?v=ujOTNg17LjI&list=PLQVvvaa0QuDdLkP8MrOXLe_rKuf6r80KO&index=1
+'''
+
+import pygame
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+
+crashed = False
+
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+
+    pygame.display.update()
+    clock.tick(30)
+
+pygame.quit()
+quit()
